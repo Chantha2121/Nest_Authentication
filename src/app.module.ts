e21ac568573e31bd8276a76ac409a0a91d29c7de
Eventlistener <chanfcsg334@gmail.com>
2024-10-22 12:51:26 +0700

    use dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,26 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3312,
-      username: 'root',
-      password: '1234',
-      database: 'Nest',
-      entities: [User],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_host'),
+        port: +configService.get('DB_port'),
+        username: configService.get('DB_user'),
+        password: configService.get('DB_password'),
+        database: configService.get('DB_name'),
+        entities: [User],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
