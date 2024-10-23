b76c4d5313201c3a0d5ce744e21b20d98878a7fb
Eventlistener <chanfcsg334@gmail.com>
2024-10-23 13:59:29 +0700

    finish register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ){}
+    async register(name: string, email: string, password: string){
+        const exitEmail = await this.userRepository.findOne({where: {email}})
+        if(exitEmail){
+            throw new Error("User already registered")
+        }
+        const salt = await bcrypt.getSalt();
+        const hashpassword = await bcrypt.hashpassword(password, salt);
+        const newUser = this.userRepository.create({
+            username: name,
+            password: hashpassword,
+            email: email
+        })
+        return this.userRepository.save(newUser);
+    }
+}
